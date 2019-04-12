f010f8792051bf2024722a7694f9f74dc3771227
Marcel Starczyk <marcel.starczyk@gmail.com>
2019-04-12 08:11:11 +0200

    Models renamed to Actions, configurator improvements
    Configurator no longer returns configured view controller as it is not really necessary and mandates boilerplate code during module configuration

--- Templates/Module.xctemplate/UIViewController/___FILEBASENAME___Configurator.swift
+++ Templates/Module.xctemplate/UIViewController/___FILEBASENAME___Configurator.swift
@@ -9,18 +9,16 @@
 import UIKit
 
 protocol ___VARIABLE_sceneName___ConfiguratorProtocol {
-    func configure___VARIABLE_sceneName___Module(with viewController: ___VARIABLE_sceneName___ViewController?) -> ___VARIABLE_sceneName___ViewController?
+    func configure___VARIABLE_sceneName___Module(with viewController: ___VARIABLE_sceneName___ViewController?)
 }
 
 final class ___VARIABLE_sceneName___Configurator: ___VARIABLE_sceneName___ConfiguratorProtocol {
-    func configure___VARIABLE_sceneName___Module(with viewController: ___VARIABLE_sceneName___ViewController?) -> ___VARIABLE_sceneName___ViewController? {
+    func configure___VARIABLE_sceneName___Module(with viewController: ___VARIABLE_sceneName___ViewController?) {
         let presenter = ___VARIABLE_sceneName___Presenter(viewController: viewController)
         let interactor = ___VARIABLE_sceneName___Interactor(presenter: presenter)
         let router = ___VARIABLE_sceneName___Router(viewController: viewController, dataStore: interactor)
         
         viewController?.interactor = interactor
         viewController?.router = router
-
-        return viewController
     }
 }
